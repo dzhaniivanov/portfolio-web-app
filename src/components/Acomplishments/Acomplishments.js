a3123aed36f7051b9435ad3b3a7dd9d989a88cbd
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-10 15:59:00 +0300

    work on timeline component

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
